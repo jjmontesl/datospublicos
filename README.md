47f33745e653034f2f085e3b8448353c7de01ace
Jose Juan Montes <jjmontes@gmail.com>
2016-04-13 03:15:54 +0200

    Migrating configuration to YAML.

---
+++ README.md
@@ -0,0 +1,9 @@
+DatosPublicos
+=============
+
+Introduction
+------------
+
+Running ETL processes
+---------------------
+
--- data/contratosdegalicia/get-pcpg.sh
+++ data/contratosdegalicia/get-pcpg.sh
@@ -3,12 +3,14 @@
 mkdir -p data
 
 #for i in {7143..19855} ; do
-for i in {19855..25000} ; do
+#for i in {7143..28000} ; do
+for i in {25729..25730} ; do
 	if [ ! -f "data/licitacion-$i.html" ] ; then
-	        wget --no-check-certificate "http://www.contratosdegalicia.es/licitacion?N=$i" -O "data/licitacion-$i.html"
+	        wget --no-check-certificate "http://www.contratosdegalicia.es/licitacion?N=$i" -O "data/licitacion-$i.html" #\
+	#        	-U "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:35.0) Gecko/20100101 Firefox/35.0"
 	fi
 	
-	grep -q 'Intentouse acceder a un concurso que non existe' data/licitacion-$i.html
+	grep -q 'O procedemento indicado non existe ou xa' data/licitacion-$i.html
 	if [ $? -eq 0 ] ; then
 		rm data/licitacion-$i.html
 	fi
